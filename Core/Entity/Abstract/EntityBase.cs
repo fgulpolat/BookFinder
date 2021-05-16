9f4e3bfa37c6e949cfab05610c595d9900090311
fgulpolat <49431870+fgulpolat@users.noreply.github.com>
2021-05-16 21:56:16 +0300

    Entity and Core layer added the project.

---
+++ Core/Entity/Abstract/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entity.Abstract
+{
+   public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual bool IsActive { get; set; } = true;
+        public virtual bool IsDeleted { get; set; } = false;
+    }
+}
